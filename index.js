c5256b7372273454acb2b583ebe752377d5361dc
testsoftileo <125642382+testsoftileo@users.noreply.github.com>
2023-02-17 17:05:07 +0500

    21

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import fetch from "node-fetch";
 let app = express();
 app.post("/getWalmartCredentials", function(request, response){
 
-	console.log(request.query['test']);
+	console.log(request.query);
 	
 	let json_response = {};
 	if(request.query['token'] != 'undefined' && request.query['consumerID'] != 'undefined' && request.query['privateKey'] != 'undefined' && request.query['keyVersion'] != 'undefined'){
