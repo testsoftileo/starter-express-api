aed2fd7bd38350c79209d70eb4a402252013c110
testsoftileo <125642382+testsoftileo@users.noreply.github.com>
2023-02-17 19:41:51 +0500

    42

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import fetch from "node-fetch";
 
 let app = express();
 // express.json()
-app.use(express.json())
+app.use(express.json({extended: true}))
 
 // express.urlencoded()
-app.use(express.urlencoded())
+// app.use(express.urlencoded())
 
 app.post("/getWalmartCredentials", function(request, response){
 
